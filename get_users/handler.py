40ab3865d00e72c02065244626365a79889e442f
Edinson Gutierrez <edison.gutierrez-ext@ab-inbev.com>
2026-05-11 16:24:06 -0500

    fix: fix: refactor get user lambda to modularize the logic

--- get_users/handler.py
+++ get_users/handler.py
@@ -1,32 +1,18 @@
-import os
-import json
-import boto3
-from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from mangum import Mangum
+from shared_package.dynamodb import DynamoDB
+from shared_package.config import settings
 
 
-load_dotenv()
 app = FastAPI()
 
-dynamodb_client_params = {}
-
-if os.getenv("IS_OFFLINE"):
-    dynamodb_client_params = {
-        "region_name": "localhost",
-        "endpoint_url": "http://localhost:8000",
-        "aws_access_key_id": "MockAccessKeyId",
-        "aws_secret_access_key": ""
-    }
-
-dynamodb = boto3.resource("dynamodb", **dynamodb_client_params)
-table = dynamodb.Table("usersTable")
 
 @app.get("/users")
 async def get_users():
     try:
-        response = table.scan()
+        dynamo = DynamoDB()
+        response: dict = dynamo.get_items(settings.DYNAMODB_TABLE_NAME)
         return JSONResponse(
             status_code=200,
             content={ "body": response }
